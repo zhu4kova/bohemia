a590864f38ff39c23e4aeea6d75daf5f14353a13
KocaH <halil.koca@students.ffhs.ch>
2017-12-11 13:17:29 +0100

    no changes
    wird überschrieben durch Anja

--- src/Startseite.java
+++ src/Startseite.java
@@ -38,7 +38,7 @@ public class Startseite {
 			public void run() {
 				try {
 					Startseite window = new Startseite();
-					window.getFrame().setVisible(true);
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -57,70 +57,48 @@ public class Startseite {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		setFrame(new JFrame());
-		getFrame().setIconImage(Toolkit.getDefaultToolkit().getImage(Startseite.class.getResource("/Bilder/logo@2x.png")));
-		getFrame().setBackground(Color.LIGHT_GRAY);
-		getFrame().setBounds(100, 100, 591, 483);
-		getFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame = new JFrame();
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\annaz\\Pictures\\logo@2x.png"));
+		frame.setBackground(Color.LIGHT_GRAY);
+		frame.setBounds(100, 100, 591, 483);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JMenuBar menuBar = new JMenuBar();
-		getFrame().setJMenuBar(menuBar);
+		frame.setJMenuBar(menuBar);
 		
 		JMenuItem mntmStartseite = new JMenuItem("Startseite");
 		menuBar.add(mntmStartseite);
-		getFrame().getContentPane().setLayout(new MigLayout("", "[50%][33.45%][100][50%]", "[88.00px][66.00][107.00][60.00px][30.00]"));
+		frame.getContentPane().setLayout(new MigLayout("", "[50%][33.45%][100][50%]", "[88.00px][66.00][107.00][60.00px][30.00]"));
 		
 		JLabel lblWillkommen = new JLabel("Bohemia - Literaturverwaltung");
 		lblWillkommen.setForeground(new Color(178, 34, 34));
 		lblWillkommen.setFont(new Font("Arial", Font.BOLD, 24));
-		getFrame().getContentPane().add(lblWillkommen, "cell 0 0 4 1");
+		frame.getContentPane().add(lblWillkommen, "cell 0 0 4 1");
 		
 		JButton btnPersonErfassen = new JButton("Person erfassen");
 		btnPersonErfassen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				Person_erfassen window = new Person_erfassen();
-				window.getPersonErfassenFrame().setVisible(true);
 			}
 		});
 		btnPersonErfassen.setBackground(UIManager.getColor("Button.background"));
 		btnPersonErfassen.setForeground(new Color(178, 34, 34));
 		btnPersonErfassen.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnPersonErfassen.setIcon(new ImageIcon(Startseite.class.getResource("/Bilder/verification (2).png")));
-		getFrame().getContentPane().add(btnPersonErfassen, "cell 0 1,grow");
+		btnPersonErfassen.setIcon(new ImageIcon("C:\\Users\\annaz\\Downloads\\verification (2).png"));
+		frame.getContentPane().add(btnPersonErfassen, "cell 0 1,grow");
 		
-		JButton btnModulErfassen = new JButton("Modul erfassen");
-		btnModulErfassen.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				Modul_erfassen window = new Modul_erfassen();
-				window.getModulErfassenFrame().setVisible(true);
-			}
-		});
-		btnModulErfassen.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnModulErfassen.setBackground(UIManager.getColor("Button.background"));
-		btnModulErfassen.setForeground(new Color(178, 34, 34));
-		btnModulErfassen.setIcon(new ImageIcon(Startseite.class.getResource("/Bilder/science-book (1).png")));
-		getFrame().getContentPane().add(btnModulErfassen, "cell 3 1,grow");
+		JButton btnNewButton = new JButton("Modul erfassen");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnNewButton.setBackground(UIManager.getColor("Button.background"));
+		btnNewButton.setForeground(new Color(178, 34, 34));
+		btnNewButton.setIcon(new ImageIcon("C:\\Users\\annaz\\Downloads\\science-book (1).png"));
+		frame.getContentPane().add(btnNewButton, "cell 3 1,grow");
 		
 		JButton btnLiteraturErfassen = new JButton("Literatur erfassen");
-		btnLiteraturErfassen.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				Literatur_erfassen window = new Literatur_erfassen();
-				window.getLiteraturErfassenFrame().setVisible(true);
-			}
-		});
 		btnLiteraturErfassen.setBackground(UIManager.getColor("Button.background"));
 		btnLiteraturErfassen.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnLiteraturErfassen.setForeground(new Color(178, 34, 34));
-		btnLiteraturErfassen.setIcon(new ImageIcon(Startseite.class.getResource("/Bilder/library (1).png")));
-		getFrame().getContentPane().add(btnLiteraturErfassen, "cell 0 2,grow");
+		btnLiteraturErfassen.setIcon(new ImageIcon("C:\\Users\\annaz\\Downloads\\library (1).png"));
+		frame.getContentPane().add(btnLiteraturErfassen, "cell 0 2,grow");
 
-	}
-
-	public JFrame getFrame() {
-		return frame;
-	}
-
-	public void setFrame(JFrame frame) {
-		this.frame = frame;
 	}
 }
